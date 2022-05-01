6308f7289dca56de78c7bc48283ad97725ac70cf
robotsch <christianhumble22@gmail.com>
2022-05-01 14:59:39 -0400

    complete 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+Employee.validates :first_name, presence: true
+Employee.validates :last_name, presence: true
+Employee.validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+Employee.validates :store_id, presence: true
+
+Store.validates :name, length: { minimum: 3 }
+Store.validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+
+@name = gets.chomp
+
+newStore = Store.create!(name: @name)
